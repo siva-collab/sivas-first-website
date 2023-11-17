1c6d69d42b161e457b67d8167d735547fb4f2158
siva-collab <101644016+siva-collab@users.noreply.github.com>
2023-11-17 18:24:45 +0000

    Dynamic data and api route

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': "Data Analyst",
+    'location': "Coimbatore",
+    'salary': "1,00,000"
+}, {
+    'id': 2,
+    'title': "Data Engineer",
+    'location': "Erode",
+}, {
+    'id': 3,
+    'title': "Data Scientist",
+    'location': "Chennai",
+    'salary': "1,50,000"
+}, {
+    'id': 4,
+    'title': "Data Manager",
+    'location': "Noida",
+    'salary': "1,25,000"
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS, company_name='Siva')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
